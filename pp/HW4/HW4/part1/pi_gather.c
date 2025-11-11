ed32c4026bfa89d9757b6f486f36d0a194c1d921
Brian <brian030128@gmail.com>
2025-11-11 18:24:04 +0800

    update

--- pp/HW4/HW4/part1/pi_gather.c
+++ pp/HW4/HW4/part1/pi_gather.c
@@ -5,6 +5,8 @@
 #include <time.h>
 #include <unistd.h>
 
+#define SEED 12345678
+
 int main(int argc, char **argv)
 {
     // --- DON'T TOUCH ---
@@ -15,13 +17,44 @@ int main(int argc, char **argv)
     int world_rank, world_size;
     // ---
 
-    // TODO: MPI init
+    // init MPI
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    // TODO: use MPI_Gather
+    long long int number_in_circle = 0;
+    long long int iterations_per_process = tosses / world_size;
+    srand(world_rank * SEED);
+
+    for (long long int i = 0; i < iterations_per_process; i++) {
+        double x = (double)rand() / RAND_MAX;
+        double y = (double)rand() / RAND_MAX;
+        if (x * x + y * y <= 1.0) {
+            number_in_circle++;
+        }
+    }
+    
+    // Create a buffer on the root process to hold the gathered counts
+    long long int *gather_buffer = NULL;
+    if (world_rank == 0) {
+        gather_buffer = malloc(world_size * sizeof(long long int));
+    }
+
+    // Gather all the 'number_in_circle' counts from each process to the root process
+    MPI_Gather(&number_in_circle, 1, MPI_LONG_LONG, gather_buffer, 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
 
     if (world_rank == 0)
     {
-        // TODO: PI result
+        // Aggregate the intermediate counts from the gather buffer
+        long long int total_number_in_circle = 0;
+        for (int i = 0; i < world_size; i++) {
+            total_number_in_circle += gather_buffer[i];
+        }
+
+        // Free the buffer
+        free(gather_buffer);
+
+        // Process PI result
+        pi_result = 4.0 * total_number_in_circle / (double)tasses;
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
