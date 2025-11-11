33312049cfec4fd6b7fd1e8c1dbd02c4cb0b6669
Brian <brian030128@gmail.com>
2025-11-11 17:27:09 +0800

    complete hello

--- pp/HW4/HW4/part1/pi_block_linear.c
+++ pp/HW4/HW4/part1/pi_block_linear.c
@@ -15,20 +15,44 @@ int main(int argc, char **argv)
     int world_rank, world_size;
     // ---
 
-    // TODO: init MPI
+    // init MPI
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
+    long long int number_in_circle = 0;
+    long long int iterations_per_process = tosses / world_size;
+    srand(world_rank * SEED);
+
+    for (long long int i = 0; i < iterations_per_process; i++) {
+        double x = (double)rand() / RAND_MAX;
+        double y = (double)rand() / RAND_MAX;
+        if (x * x + y * y <= 1.0) {
+            number_in_circle++;
+        }
+    }
     if (world_rank > 0)
     {
-        // TODO: handle workers
+        // Workers send their intermediate count to the master process (rank 0)
+        MPI_Send(&number_in_circle, 1, MPI_LONG_LONG, 0, 0, MPI_COMM_WORLD);
     }
     else if (world_rank == 0)
     {
-        // TODO: main
+        // Master process receives intermediate counts from all workers
+        long long int total_number_in_circle = number_in_circle;
+        long long int received_count;
+        for (int i = 1; i < world_size; i++) {
+            MPI_Recv(&received_count, 1, MPI_LONG_LONG, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            total_number_in_circle += received_count;
+        }
+        number_in_circle = total_number_in_circle;
     }
 
     if (world_rank == 0)
     {
-        // TODO: process PI result
+        // Process PI result
+        pi_result = 4.0 * number_in_circle / (double)tosses;
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
