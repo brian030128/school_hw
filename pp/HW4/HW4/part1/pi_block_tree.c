3c1417a8c845bf4c353e3135491d496399bae74a
Brian <brian030128@gmail.com>
2025-11-11 18:01:11 +0800

    update

--- pp/HW4/HW4/part1/pi_block_tree.c
+++ pp/HW4/HW4/part1/pi_block_tree.c
@@ -11,73 +11,82 @@ int main(int argc, char **argv)
     MPI_Init(&argc, &argv);
     double start_time = MPI_Wtime();
     double pi_result;
-    long long int tosses = atoll(argv[1]); // Use atoll for long long int
+    long long int tosses = atoll(argv[1]);
     int world_rank, world_size;
     // ---
 
-    // TODO: MPI init
+    // MPI init
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    // Each process calculates its own partial number of points in the circle.
-    long long int tosses_per_process = tosses / world_size;
-    long long int number_in_circle = 0;
+    // Each process calculates its own partial number of points in the circle
+    long long int local_tosses = tosses / world_size;
+    long long int local_number_in_circle = 0;
     
-    // Seed the random number generator uniquely for each process.
+    // Seed the random number generator to be unique for each process
     srand(time(NULL) + world_rank);
 
-    for (long long int i = 0; i < tosses_per_process; i++)
-    {
+    for (long long int toss = 0; toss < local_tosses; toss++) {
         double x = (double)rand() / RAND_MAX;
         double y = (double)rand() / RAND_MAX;
         double distance_squared = x * x + y * y;
-        if (distance_squared <= 1)
-        {
-            number_in_circle++;
+        if (distance_squared <= 1) {
+            local_number_in_circle++;
         }
     }
 
-    // TODO: binary tree redunction
-    // The reduction happens in log2(world_size) steps.
-    // In each step, the distance between communicating processes doubles.
-    for (int distance = 1; distance < world_size; distance *= 2)
-    {
-        // A process is a "sender" if its rank is in the upper half of a sub-group.
-        // The condition (world_rank % (2 * distance)) == distance identifies these processes.
-        if ((world_rank % (2 * distance)) == distance)
-        {
-            int destination = world_rank - distance;
-            MPI_Send(&number_in_circle, 1, MPI_LONG_LONG, destination, 0, MPI_COMM_WORLD);
-            // Once a process has sent its data, it becomes inactive for the rest of the reduction.
-            break;
-        }
-        // A process is a "receiver" if its rank is the base of a sub-group.
-        // The condition (world_rank % (2 * distance)) == 0 identifies these processes.
-        else if ((world_rank % (2 * distance)) == 0)
-        {
-            // The receiver must ensure its sending partner exists.
-            // This is always true in a power-of-two scenario but is good practice.
-            int source = world_rank + distance;
-            if (source < world_size)
-            {
-                long long int received_count;
-                MPI_Recv(&received_count, 1, MPI_LONG_LONG, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                number_in_circle += received_count;
-            }
-        }
-    }
+    if (world_rank == 0) {
+        long long int total_number_in_circle = local_number_in_circle;
 
+        // --- TODO: Non-blocking receives ---
+        // Allocate arrays for requests and the received data from worker processes.
+        MPI_Request* requests = (MPI_Request*)malloc(sizeof(MPI_Request) * (world_size - 1));
+        long long int* received_counts = (long long int*)malloc(sizeof(long long int) * (world_size - 1));
 
-    if (world_rank == 0)
-    {
-        // TODO: PI result
-        // After the reduction, rank 0 has the total count from all processes.
-        pi_result = 4.0 * number_in_circle / tosses;
+        // Issue a non-blocking receive for each worker process (ranks 1 to world_size-1).
+        for (int i = 1; i < world_size; i++) {
+            MPI_Irecv(
+                &received_counts[i - 1], // Buffer for incoming data from rank i
+                1,                       // Count of elements
+                MPI_LONG_LONG,           // Datatype of the elements
+                i,                       // Source rank
+                0,                       // Message tag
+                MPI_COMM_WORLD,          // Communicator
+                &requests[i - 1]         // MPI_Request handle
+            );
+        }
+
+        // Wait for all the non-blocking receive operations to complete.
+        MPI_Waitall(world_size - 1, requests, MPI_STATUSES_IGNORE);
+
+        // Aggregate the results from all workers.
+        for (int i = 0; i < world_size - 1; i++) {
+            total_number_in_circle += received_counts[i];
+        }
+
+        // Clean up allocated memory
+        free(requests);
+        free(received_counts);
+        // ---
+
+        // PI result
+        pi_result = 4.0 * total_number_in_circle / ((double)tosses);
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
         printf("%lf\n", pi_result);
         printf("MPI running time: %lf Seconds\n", end_time - start_time);
+        // ---
+    } else {
+        // --- TODO: All other ranks send their results to rank 0 ---
+        MPI_Send(
+            &local_number_in_circle, // Data to send
+            1,                       // Count of elements
+            MPI_LONG_LONG,           // Datatype
+            0,                       // Destination rank (root)
+            0,                       // Message tag
+            MPI_COMM_WORLD           // Communicator
+        );
         // ---
     }
 
