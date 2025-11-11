c7210443903c22d684e810badc00ccf05bbe8ffc
Brian <brian030128@gmail.com>
2025-11-11 18:29:40 +0800

    update

--- pp/HW4/HW4/part1/pi_reduce.c
+++ pp/HW4/HW4/part1/pi_reduce.c
@@ -5,6 +5,8 @@
 #include <time.h>
 #include <unistd.h>
 
+#define SEED 12345678
+
 int main(int argc, char **argv)
 {
     // --- DON'T TOUCH ---
@@ -15,13 +17,32 @@ int main(int argc, char **argv)
     int world_rank, world_size;
     // ---
 
-    // TODO: MPI init
+    // init MPI
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    // TODO: use MPI_Reduce
+    long long int number_in_circle = 0;
+    long long int iterations_per_process = tosses / world_size;
+    unsigned int seed = world_rank * time(0);
+
+    for (long long int i = 0; i < iterations_per_process; i++) {
+        double x = (double)rand_r(&seed) / RAND_MAX;
+        double y = (double)rand_r(&seed) / RAND_MAX;
+        if (x * x + y * y <= 1.0) {
+            number_in_circle++;
+        }
+    }
+
+    // A variable to store the result of the reduction on the root process
+    long long int total_number_in_circle;
+
+    // Use MPI_Reduce to sum all 'number_in_circle' counts into 'total_number_in_circle' on the root process
+    MPI_Reduce(&number_in_circle, &total_number_in_circle, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
 
     if (world_rank == 0)
     {
-        // TODO: PI result
+        // Process PI result using the final aggregated count
+        pi_result = 4.0 * total_number_in_circle / (double)tosses;
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
